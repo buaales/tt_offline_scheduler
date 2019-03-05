53a53a34fba77bb95ec123b036a3e699f8b9d7ad
mwiacx <759046501@qq.com>
2019-03-05 09:46:48 +0800

    sync

--- scheduler/model.py
+++ scheduler/model.py
@@ -1,13 +1,78 @@
-"调度器模型"
+"""调度器模型"""
 
-class Task:
+from msg_scheduler import model
 
-    _node_id = 0
-    _task_id = 0
+class Task(model.NamedObj):
+    """端节点上的可执行任务"""
 
-    def __init__(self, offset: int, wect: int, deadline: int, peroid: int, delta: float)
-        self._offset: int = offset
-        self._wect: int = wect
-        self._deadline: int = deadline
-        self._peroid: int = peroid
-        self._delta: float = delta
+    def __init__(self, name: str, node_id: int, task_id: int):
+        super().__init__(name)
+        self._node_id = node_id
+        self._task_id = task_id
+    # offset
+    @property
+    def offest(self) -> int:
+        return self._offset
+    @offset.setter
+    def offset(self, offset:int):
+        self._offset = offset
+    # wcet
+    @property
+    def wcet(self) -> int:
+        return self._wcet
+    @wcet.setter
+    def wcet(self, wcet:int):
+        self._wect = wcet
+    # deadline
+    @property
+    def deadline(self) -> int:
+        return self._deadline
+    @deadline.setter
+    def deadline(self, deadline: int):
+        self.deadline = deadline
+    #peroid
+    @property
+    def peroid(self) -> int:
+        return self._peroid
+    @peroid.setter
+    def peroid(self, peroid:int):
+        self._peroid = peroid
+
+class FreeTask(Task):
+    """通信无关的实时任务"""
+
+    def __init__(self, name: str, node_id: int, task_id: int):
+        super().__init__(name, node_id, task_id)
+
+class CommTask(Task):
+    """通信类是实时任务"""
+
+    def __init__(self, name: str, node_id: int, task_id: int):
+        super().__init__(name, node_id, task_id)
+
+    @property
+    def delta(self) -> float:
+        return self._delta
+    @delta.setter
+    def delta(self, delta):
+        self._delta = delta
+
+class ProducerTask(CommTask):
+    """生产者实时通信类任务"""
+
+    def __init__(self, name: str, node_id: int, task_id: int):
+        super().__init__(name, node_id, task_id)
+
+
+class CustomerTask(CommTask):
+    """消费者实时通信类任务"""
+
+    def __init__(self, name: str, node_id: int, task_id: int):
+        super().__init__(name, node_id, task_id)
+
+
+class ShaperTask(CommTask):
+    """Shaper实时通信类任务"""
+
+    def __init__(self, name: str, node_id: int, task_id: int):
+        super().__init__(name, node_id, task_id)
--- tests/bench_gen.py
+++ tests/bench_gen.py
@@ -1,13 +1,21 @@
 "测试集生成模块"
 
+from msg_scheduler import model as mmodel
+from scheduler import model as tmodel
 import time
 
-def gen_model(periods, utilization, granuolarity):
+def gen_model(periods: int, utilization: float, granuolarity: int):
     '''
     功能描述：
         生成测试集
     参数描述：
-        peroids: 任务周期集合
+        peroids: 任务周期集合, 单位：毫秒
         utilization: 任务集总体利用率
         granuolarity: 任务调度时间粒度，单位：微秒
     '''
+    # Check utilization
+    if (utilization < 0) or (utilization > 1):
+        print("[Benchmark Gen][ERR]: utilization wrong value")
+        return None
+
+    
