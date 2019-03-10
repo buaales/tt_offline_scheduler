1c4dccc1744058a331a4fec94dad21e4c1ebdba7
mwiacx <759046501@qq.com>
2019-03-10 19:18:38 +0800

    temp update

--- tests/bench_gen.py
+++ tests/bench_gen.py
@@ -1,8 +1,11 @@
-"测试集生成模块"
+
 
 from msg_scheduler import model as mmodel
 from scheduler import model as tmodel
+import sys
 import time
+import random
+import networkx
 
 def gen_network_large(network: mmodel.Network):
     pass
@@ -11,7 +14,34 @@ def gen_network_medium(network: mmodel.Network):
     pass
 
 def gen_network_small(network: mmodel.Network):
-    pass
+    # 1. generate a random tree and select one with 4 endnode
+
+    enodenum = 0
+    while enodenum != 4:
+        # gen
+        seed = random.randint(1, 9999)
+        g = networkx.generators.random_tree(6, seed)
+        network = mmodel.Network()
+        # check the number of end-node
+        for n in g.nodes():
+            nei = list(g.neighbors(n))
+            if len(nei) == 1:
+                enodenum += 1
+    
+    # generate network
+    node_name_map = {}
+    for n in g.nodes():
+        nei = list(g.neighbors(n))
+        if len(nei) == 1:
+            new_node = mmodel.EndNode('app_{n}')
+        else:
+            new_node = mmodel.SwitchNode('msg_{n}')
+        network.add_node(new_node)
+        node_name_map[n] = new_node.name
+    for e in g.edges:
+        network.add_link(node_name_map[e[0]], node_name_map[e[1]])
+
+    return
 
 def gen_network(net_type: int):
     network = mmodel.Network()
@@ -44,11 +74,19 @@ def gen_model(periods: int, utilization: float, granuolarity: int, net_type: int
         return None
 
     # 1. Gen a network
-    network: mmodel.Network = gen_network(net_type)
+    network = gen_network(net_type)
 
     # 2. Gen App Sets
     # 遍历 Node节点生成任务集合
-    task_sets = gen_task_set(periods: int, utilization: float, granuolarity: int)
+    #task_sets = gen_task_set(periods, utilization, granuolarity)
 
     # 3. 建立通信链路
-    gen_virtual_links()
+    #gen_virtual_links()
+
+# Test
+if __name__ == "__main__":
+    while True:
+        start = time.time()
+        gen_network(0)
+        stop = time.time()
+        print("time: %d", stop - start)
