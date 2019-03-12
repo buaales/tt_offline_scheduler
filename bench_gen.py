1f793159093f6f4bf669e04ff9247a06e7442a29
mwiacx <759046501@qq.com>
2019-03-12 11:40:45 +0800

    update network generation

--- bench_gen.py
+++ bench_gen.py
@@ -4,11 +4,88 @@ import sys
 import time
 import random
 import networkx
+import matplotlib.pyplot as plt
 
 from msg_scheduler import model as mmodel
 from scheduler import model as tmodel
 
-def gen_network_common(network: mmodel.Network, g: networkx.Graph):
+def gen_network_large(network: mmodel.Network):
+    # 1. generate a random tree and select one with 8 end-node
+    enode_num = 0
+    while enode_num < 40:
+        enode_num = 0
+        g = networkx.generators.random_tree(72)
+        # check the number of end-node
+        for n in g.nodes():
+            if networkx.degree(g, n) == 1:
+                enode_num += 1
+    print("get one wiht endnode_num %d" % enode_num)
+    # 1.1 write into file
+    networkx.readwrite.graphml.write_graphml(g, 
+        "./output/graph_large_gen/graph_{}.graphml".format(idx))
+
+    return
+
+def gen_network_medium(network: mmodel.Network, idx: int):
+    # 1. generate a random tree and select one with 8 end-node
+    enode_num = 0
+    while enode_num != 16:
+        enode_num = 0
+        g = networkx.generators.random_tree(24)
+        # check the number of end-node
+        for n in g.nodes():
+            if networkx.degree(g, n) == 1:
+                enode_num += 1
+    print("get one wiht endnode_num %d" % enode_num)
+    # 1.1 write into file
+    networkx.readwrite.graphml.write_graphml(g, 
+        "./output/graph_medium_gen/graph_{}.graphml".format(idx))
+
+    return
+
+def gen_network_small(network: mmodel.Network, idx: int):
+    # 1. generate a random tree and select one with 4 endnode
+    enode_num = 0
+    while enode_num != 4:
+        enode_num = 0
+        g = networkx.generators.random_tree(6)
+        # check the number of end-node
+        for n in g.nodes():
+            nei = list(g.neighbors(n))
+            if len(nei) == 1:
+                enode_num += 1
+    print("get one wiht endnode_num %d" % enode_num)
+    # 1.1 write into file
+    networkx.readwrite.graphml.write_graphml(g, 
+        "./output/graph_small_gen/graph_{}.graphml".format(idx))
+
+    return
+
+def gen_network(net_type: int, idx: int):
+    network = mmodel.Network()
+    # 1. calls sub-function
+    if net_type == 0:
+        gen_network_small(network, idx)
+    elif net_type == 1:
+        gen_network_medium(network, idx)
+    elif net_type == 2:
+        gen_network_large_star(network, idx)
+    else:
+        print("[Benchmark Gen][ERR]: Unkown network type")
+        return None
+    # 2. return network
+    return network
+
+def gen_network_into_file(net_type: int):
+    idx = 0
+    while idx < 1000:
+        start = time.time()
+        gen_network(net_type, idx)
+        idx += 1
+        stop = time.time()
+        print("spend time: %f" % (stop - start))
+
+def gen_network_from_file(network: mmodel.Network, g: networkx.Graph):
     # generate network
     node_name_map = {}
     for n in g.nodes():
@@ -23,88 +100,6 @@ def gen_network_common(network: mmodel.Network, g: networkx.Graph):
         network.add_link(node_name_map[e[0]], node_name_map[e[1]])
 
     return
-
-def gen_network_large(network: mmodel.Network):
-    # 1. generate a random tree and select one with 8 end-node
-    enode_num = 0
-    seed = 0
-    while enode_num < 30:
-        enode_num = 0
-        g = networkx.generators.random_tree(56, seed)
-        # check the number of end-node
-        for n in g.nodes():
-            if networkx.degree(g, n) == 1:
-                enode_num += 1
-        print('enode_num %d %d' % (enode_num, seed))
-        seed += 1
-
-    while True:
-        g = networkx.generators.random_tree(56, seed-1)
-        print(enode_num)
-
-    print("get one wiht endnodenum %d" % enode_num)
-    # 2. generate a network
-    gen_network_common(network, g)
-    return
-
-def gen_network_large_star(network: mmodel.Network):
-    # 1. generate a random tree and select one with 48 end-node
-    enode_num = 0
-    g = networkx.generators.classic.star_graph(48)
-    # check the number of end-node
-    for n in g.nodes():
-        if networkx.degree(g, n) == 1:
-            enode_num += 1
-    print("get one wiht endnodenum %d" % enode_num)
-    # 2. generate a network
-    gen_network_common(network, g)
-    return
-
-def gen_network_medium(network: mmodel.Network):
-    # 1. generate a random tree and select one with 8 end-node
-    enode_num = 0
-    while enode_num != 16:
-        enode_num = 0
-        g = networkx.generators.random_tree(24)
-        # check the number of end-node
-        for n in g.nodes():
-            if networkx.degree(g, n) == 1:
-                enode_num += 1
-    print("get one wiht endnodenum %d" % enode_num)
-    # 2. generate a network
-    gen_network_common(network, g)
-    return
-
-def gen_network_small(network: mmodel.Network):
-    # 1. generate a random tree and select one with 4 endnode
-    enode_num = 0
-    while enode_num != 4:
-        enode_num = 0
-        g = networkx.generators.random_tree(6)
-        # check the number of end-node
-        for n in g.nodes():
-            nei = list(g.neighbors(n))
-            if len(nei) == 1:
-                enode_num += 1
-    print("get one wiht endnodenum %d" % enode_num)
-    # 2. generate a network
-    gen_network_common(network, g)
-    return
-
-def gen_network(net_type: int):
-    network = mmodel.Network()
-    # 1. calls sub-function
-    if net_type == 0:
-        gen_network_small(network)
-    elif net_type == 1:
-        gen_network_medium(network)
-    elif net_type == 2:
-        gen_network_large(network)
-    else:
-        print("[Benchmark Gen][ERR]: Unkown network type")
-        return None
-    # 2. return network
-    return network
 
 def gen_model(periods: int, utilization: float, granuolarity: int, net_type: int):
     '''
@@ -121,7 +116,7 @@ def gen_model(periods: int, utilization: float, granuolarity: int, net_type: int
         print("[Benchmark Gen][ERR]: utilization wrong value")
         return None
 
-    # 1. Gen a network
+    # 1. 从生成好的图像文件中获取网络
     network = gen_network(net_type)
 
     # 2. Gen App Sets
@@ -131,10 +126,21 @@ def gen_model(periods: int, utilization: float, granuolarity: int, net_type: int
     # 3. 建立通信链路
     #gen_virtual_links()
 
-# Test
+
+# 生成文本格式的图存储到文件中
+'''
 if __name__ == "__main__":
-    while True:
-        start = time.time()
-        gen_network(1)
-        stop = time.time()
-        print("time: %d", stop - start)
+    gen_network_into_file(1)
+'''
+
+# 测试生成的图是否合格
+'''
+if __name__ == "__main__":
+    idx = random.randint(0, 100)
+    file_name = './output/graph_medium_gen/graph_{}.graphml'.format(idx)
+    g = networkx.readwrite.graphml.read_graphml(file_name)
+    #g = networkx.complete_graph(5)
+    print("draw graph_%d" % idx)
+    networkx.draw_networkx(g)
+    plt.show()
+'''
