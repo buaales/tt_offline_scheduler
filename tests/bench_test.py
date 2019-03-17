5a08b26b56ca1e7ad92e162445d1b856a1e50303
mwiacx <759046501@qq.com>
2019-03-17 21:53:52 +0800

    for sync

--- tests/bench_test.py
+++ tests/bench_test.py
@@ -4,8 +4,18 @@ import gen_bench as gb
 
 
 if __name__ == '__main__':
-    #
+    # setup param
     periods = [50, 75]
     util = 0.5
     gran = 1
-    network, task_dict = gen_model(periods, util, gran, 0, 0)
+    net_type = 0
+    times = 8
+    # call gen_model
+    network, task_dict = gb.gen_model(periods, util, gran, net_type, times)
+    # draw the network
+    network.draw()
+    # check param
+    f = open("../output/bench_test.txt", 'w')
+    # print param for each node
+    for node in task_dict:
+
