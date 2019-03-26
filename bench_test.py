3b54d0fd620865911002eb131842759d70eb5fba
mwiacx <759046501@qq.com>
2019-03-26 18:54:08 +0800

    for sync

--- bench_test.py
+++ bench_test.py
@@ -4,14 +4,16 @@ import gen_bench as gb
 from msg_scheduler import model as mmodel
 from msg_scheduler import constrains, analyzer
 from scheduler import model as tmodel
+from scheduler import solver as tsolver
+from gurobipy import *
 
 if __name__ == '__main__':
     # setup param
     peroids = [50, 75]
     util = 0.75
     gran = 1
-    net_type = 2
-    times = 8
+    net_type = 0
+    times = 1
     # call gen_model
     network, task_dict = gb.gen_model(peroids, util, gran, net_type, times)
     # draw the network
@@ -28,7 +30,10 @@ if __name__ == '__main__':
             real_util += task.wcet / task.peroid
             #print("per {}".format(task.peroid))
             f.writelines('task id: {}, task type: {}'.format(i, type(task)))
-            f.writelines('\toffset0: {}, wcet: {}, deadline0: {}, peroid: {}'.format(task.offset0, task.wcet, task.deadline0, task.peroid))
+            _phi0 = int(task.offset0 * 1000)
+            _d0 = int(task.deadline0 * 1000)
+            _wcet0 = int(task.wcet * 1000)
+            f.writelines('\toffset0: {}, wcet: {}, deadline0: {}, peroid: {}'.format(_phi0, _wcet0, _d0, task.peroid * 1000))
             if isinstance(task, tmodel.CommTask):
                 f.writelines(', delta: {}\n'.format(task.delta))
             else:
@@ -36,9 +41,13 @@ if __name__ == '__main__':
         f.writelines('check util: %f\n' % real_util)
 
     f.writelines("######## END ########\n")
-
     f.close()
 
+    # test solver
+    solver = tsolver.Solver(network, task_dict, util * 0.75)
+    solver.solve("./output/gurobi.txt")
+    
+    '''
     sc = mmodel.Scheduler(network)
     for node in task_dict:
         sc.add_apps(task_dict[node])
@@ -47,4 +56,5 @@ if __name__ == '__main__':
     sc.add_constrains(hook)
     df = hook.to_dataframe()
     an = analyzer.Analyzer(df, network, sc.app_lcm)
+    '''
     #an.print_by_time()
--- scheduler/solver.py
+++ scheduler/solver.py
@@ -3,7 +3,7 @@ import time
 
 from msg_scheduler import model as MModel
 from scheduler import model as TModel
-from gurobipy import Model as GModel
+from gurobipy import *
 
 class Solver:
     ''' The Solver Class '''
@@ -11,25 +11,30 @@ class Solver:
         self._network = network
         self._task_dict = task_dict
         self._free_util = free_util
-        self._solver = GModel("tt_schduler")
+        self._solver = Model("tt_schduler")
+        self._vars = {}
 
     def __build_var(self):
         '''
         Build vars for every communication task
         '''
         # build for min_max Intermediate variables
-        self._solver.addVar(vtype=GRB.INTEGER, name="max")
+        self._vars["max"] = self._solver.addVar(vtype=GRB.INTEGER, name="max")
 
         for node in self._task_dict:
             tasks = self._task_dict[node]
             for task in tasks:
                 # traverse task list in each node
                 # skip free-task
-                if isinstance(taks, TModel.FreeTask):
+                if isinstance(task, TModel.FreeTask):
                     continue
                 # phi var and deadline var
-                self._solver.addVar(vtype=GRB.INTEGER, name='{}_phi'.format(task.name))
-                self._solver.addVar(vtype=GRB.INTEGER, name='{}_deadline'.format(task.name))
+                self._vars["{}_phi".format(task.name)] = self._solver.addVar(vtype=GRB.INTEGER,
+                                                        name="{}_phi".format(task.name))
+                self._vars["{}_deadline".format(task.name)] = self._solver.addVar(vtype=GRB.INTEGER,
+                                                        name="{}_deadline".format(task.name))
+                #self._vars["{}_wcet".format(task.name)] = self._solver.addVar(vtype=GRB.INTEGER,
+                #                                        name="{}_wcet".format(task.name))
 
     def __build_constrains(self):
         '''
@@ -41,37 +46,50 @@ class Solver:
             tasks = self._task_dict[node]
             expr = -(1 - self._free_util)
             for task in tasks:
+                # skip free-task
+                if isinstance(task, TModel.FreeTask):
+                    continue
                 _phi0 = int(task.offset0 * 1000)
                 _d0 = int(task.deadline0 * 1000)
+                _wcet0 = int(task.wcet * 1000)
                 _delta = task.delta
                 # constrains for d
-                _d = s.getVarByName('{}_deadline'.format(task.name))
-                s.addConstr(_d > 0, 'c_{}_d_0'.format(task.name))
+                _d = self._vars['{}_deadline'.format(task.name)]
+                s.addConstr(_d >= 0, 'c_{}_d_0'.format(task.name))
                 s.addConstr(_d <= _d0, 'c_{}_d_1'.format(task.name))
                 # constrains for phi
-                _phi = s.getVarByName('{}_phi'.format(task.name))
+                _phi = self._vars['{}_phi'.format(task.name)]
                 s.addConstr(_phi >= _phi0, 'c_{}_phi_0'.format(task.name))
-                s.addConstr(_phi < _d, 'c_{}_phi_1'.format(task.name))
+                s.addConstr(_phi <= _d - _wcet0, 'c_{}_phi_1'.format(task.name))
                 # part of util constrains
-                expr += task.wcet / _d
+                #_wcet = self._vars['{}_wcet'.format(task.name)]
+                #s.addConstr(_wcet == _wcet0, 'c_{}_wcet'.format(task.name))
+                # expr += _wcet / _d
                 # max
-                _max = s.getVarByName('max')
-                _expr_temp = abs_((_phi - _phi0) * _delta - (_d0 - _d) * (1 - _delta)) + (_d - _phi)
+                _max = self._vars['max']
+                _expr_temp = (_phi - _phi0) * _delta - (_d0 - _d) * (1 - _delta) + _d - _phi
+                s.addConstr(_max >= _expr_temp, 'max_{}'.format(task.name))
+                _expr_temp = (_d0 - _d) * (1 - _delta) - (_phi - _phi0) * _delta + _d - _phi
                 s.addConstr(_max >= _expr_temp, 'max_{}'.format(task.name))
             # Add util constrains
-            s.addConstr(expr <= 0, 'u_{}'.format(node.name))
+            # s.addConstr(expr <= 0, 'u_{}'.format(node.name))
 
         # set objective: min_max
-        _max = s.getVarByName('max')
+        _max = self._vars['max']
         s.setObjective(_max, GRB.MINIMIZE)
 
 
-    def solver(self, file_path: str):
+    def solve(self, file_path: str):
         try:
             # build var
-            __build_var()
+            self.__build_var()
+            print(len(self._solver.getVars()))
+            for v in self._solver.getVars():
+                print(3)
+                print(v.varName)
+            print(2)
             # build constrains
-            __build_constrains()
+            self.__build_constrains()
             # solver
             print("Slover started!")
             _t0 = time.clock()
@@ -83,6 +101,6 @@ class Solver:
             with open(file_path, 'w') as f:
                 for var in self._solver.getVars():
                     f.writelines('{}, {}\n'.format(var.varName, var.x))
-        except GurobiError:
-            print("Gurobi Error")
+        except GurobiError as e:
+            print("Gurobi Error: %s" % (e.message))
 
